449687f45c9d88fa04f5336b53553361b1e4e0f5
Mike <michaelsabatino6501@gmail.com>
2026-01-27 21:19:54 -0500

    Add translation table for Caesar cipher
    Removed the print function and added a translation table that maps each character of the first string with the corresponding character of the second string.

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,4 +1,4 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 shift = 5
 shifted_alphabet = alphabet[shift:] + alphabet[:shift] 
-print(shifted_alphabet)
+translation_table = str.maketrans(alphabet, shifted_alphabet)
