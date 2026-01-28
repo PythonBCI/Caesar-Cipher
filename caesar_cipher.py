3ad63d36f04e2888c054241825cf7e17d3dc1e7b
Mike <michaelsabatino6501@gmail.com>
2026-01-27 22:40:44 -0500

    Implement input validation for shift in caesar function
    Added input validation for shift parameter in caesar function. Shift must be a positive number between 1 and 25

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -6,10 +6,16 @@ text = 'hello world'
 encrypted_text = text.translate(translation_table)
 
 def caesar(text, shift):
-   alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    if not isinstance(shift, int):
+        return 'Shift must be an integer value.'
+    
+    if shift < 1 or shift > 25:
+        return 'Shift must be an integer between 1 and 25.'
+
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
     return text.translate(translation_table)
 
 encrypted_text = caesar('freeCodeCamp', 3)
-print(encrypted_text)
+print(encrypted_text))
