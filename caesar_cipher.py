a953b3a9a4949f6aa2b02321c468af5241b7d17b
Mike <michaelsabatino6501@gmail.com>
2026-01-27 22:22:27 -0500

    Enhance caesar function for case sensitivity
    Refactor caesar function to handle uppercase letters and return encrypted text using .upper.

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -6,8 +6,10 @@ text = 'hello world'
 encrypted_text = text.translate(translation_table)
 
 def caesar(text, shift):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+   alphabet = 'abcdefghijklmnopqrstuvwxyz'
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-    translation_table = str.maketrans(alphabet, shifted_alphabet)
-    encrypted_text = text.translate(translation_table)
-    print(encrypted_text)
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+    return text.translate(translation_table)
+
+encrypted_text = caesar('freeCodeCamp', 3)
+print(encrypted_text)
