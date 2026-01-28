9a96584c05e1d4cb9a32782ee7fddfb117e3dede
Mike <michaelsabatino6501@gmail.com>
2026-01-27 21:26:20 -0500

    Add Caesar cipher encryption example
    Used the translate() method to return the string that's generated with the translation_table variable

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -2,3 +2,5 @@ alphabet = 'abcdefghijklmnopqrstuvwxyz'
 shift = 5
 shifted_alphabet = alphabet[shift:] + alphabet[:shift] 
 translation_table = str.maketrans(alphabet, shifted_alphabet)
+text = 'hello world'
+encrypted_text = text.translate(translation_table)
