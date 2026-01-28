18f45dcb4c0b17750c928b0949ba748bd847a10b
Mike <michaelsabatino6501@gmail.com>
2026-01-27 23:24:24 -0500

    Refactor caesar_cipher by removing hardcoded values
    Removed hardcoded values that were given in the beginning of the tutorial.

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,10 +1,3 @@
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-shift = 5
-shifted_alphabet = alphabet[shift:] + alphabet[:shift] 
-translation_table = str.maketrans(alphabet, shifted_alphabet)
-text = 'hello world'
-encrypted_text = text.translate(translation_table)
-
 def caesar(text, shift, encrypt=True):
 
     if not isinstance(shift, int):
